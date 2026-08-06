83d598781f9da78d659c6cdc6026071f48136252
alex <alex@debian.debian>
2026-08-07 00:16:51 +0300

    использованы макросы на сравнение времени

--- src/main.c
+++ src/main.c
@@ -11,24 +11,24 @@ MODULE_AUTHOR("Konygin");
 MODULE_DESCRIPTION("Kernel timer module: 30s interval, stops after 5 mins");
 MODULE_VERSION("1.0");
 
-#define TIMER_INTERVAL_MS 30000
-#define MAX_MINUTES 5
+static const unsigned long TIMER_INTERVAL_MS = 30000;
+static const unsigned int MAX_MINUTES = 5;
 
 static struct timer_list g_timer;
-static int tick_count = 0;
+static int tick_count = 1;
+static unsigned long start_jiffies;
+static unsigned long target_time_finish;
 
 static void timer_callback(struct timer_list *t) {
-  tick_count++;
+  ++tick_count;
 
-  int minutes = tick_count / 2;
-
-  if (minutes <= MAX_MINUTES) {
-    pr_info("min=%d: Hello, timer!\n", minutes);
-    if (minutes < MAX_MINUTES) {
-      mod_timer(&g_timer, jiffies + msecs_to_jiffies(TIMER_INTERVAL_MS));
-    } else {
-      pr_info("Timer stopped: 5 minutes elapsed.\n");
-    }
+  if (time_before(jiffies, target_time_finish)) {
+    unsigned int delta = jiffies_to_msecs(jiffies - start_jiffies);
+    pr_info("min=%d: Hello, timer!\n", delta / 60000);
+    unsigned long target_time = start_jiffies + msecs_to_jiffies(TIMER_INTERVAL_MS * tick_count);
+    mod_timer(&g_timer, target_time);
+  } else {
+    pr_info("Timer stopped: 5 minutes elapsed.\n");
   }
 }
 
@@ -39,7 +39,10 @@ static int __init my_timer_init(void) {
 
   timer_setup(&g_timer, timer_callback, 0);
 
-  mod_timer(&g_timer, jiffies + msecs_to_jiffies(TIMER_INTERVAL_MS));
+  start_jiffies = jiffies;
+  target_time_finish = jiffies + msecs_to_jiffies(MAX_MINUTES * 60000);
+  unsigned long target_time = start_jiffies + msecs_to_jiffies(TIMER_INTERVAL_MS * tick_count);
+  mod_timer(&g_timer, target_time);
 
   return 0;
 }
